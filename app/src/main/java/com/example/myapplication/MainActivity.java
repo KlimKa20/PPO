930a0832fa4fa6f808414afaf9d9b0828c8a40e9
KlimKa20 <62294637+KlimKa20@users.noreply.github.com>
2020-09-11 15:38:09 +0300

    Adding fragments starting development ViewModel

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,21 +13,20 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+//View.OnClickListener,implements  AdapterView.OnItemSelectedListener
+public class MainActivity extends FragmentActivity {
 
     Spinner spinner;
     Spinner spinner2;
-    EditText editText;
-    EditText editText1;
-    ClipboardManager clipboardManager;
-    float procent;
+//    EditText editText;
+//    EditText editText1;
+//    ClipboardManager clipboardManager;
+//    float procent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         Intent intent = getIntent();
         String action = intent.getAction();
         int layoutId = 0;
@@ -42,125 +41,115 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         spinner = findViewById(R.id.spinner);
         spinner2 = findViewById(R.id.spinner2);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setOnItemSelectedListener(this);
-        spinner2.setOnItemSelectedListener(this);
+
         spinner.setAdapter(adapter);
         spinner2.setAdapter(adapter);
-        editText = (EditText) findViewById(R.id.etNum);
-        editText1 = (EditText) findViewById(R.id.etNum1);
-        clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        //        spinner.setOnItemSelectedListener(this);
+//        spinner2.setOnItemSelectedListener(this);
+//        editText = (EditText) findViewById(R.id.etNum);
+//        editText1 = (EditText) findViewById(R.id.etNum1);
+//        clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String temp = spinner.getSelectedItem().toString() + spinner2.getSelectedItem().toString();
-        int strId = getResources().getIdentifier(temp, "string", getPackageName());
-        procent = Float.parseFloat(getString(strId));
-        String char0 = editText.getText().toString();
-        if (char0.length() != 0)
-            editText1.setText(Float.toString(Float.parseFloat(char0) * procent));
-//        BufferedReader reader=null;
-//        try {
-//            HttpURLConnection connection = (HttpURLConnection) new URL("https://api.exchangeratesapi.io/latest?base="+spinner.getSelectedItem()+"&&symbols="+spinner2.getSelectedItem()).openConnection();
-//            connection.setRequestMethod("GET");
-//            connection.connect();
-//            reader= new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//            StringBuilder buf=new StringBuilder();
-//            String output = buf.toString();
-//            procent= Integer.parseInt(output.substring(output.indexOf(spinner.getSelectedItem().toString())+2,output.indexOf("}")));
+//    @Override
+//    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//        String temp = spinner.getSelectedItem().toString() + spinner2.getSelectedItem().toString();
+//        int strId = getResources().getIdentifier(temp, "string", getPackageName());
+//        procent = Float.parseFloat(getString(strId));
+//        String char0 = editText.getText().toString();
+//        if (char0.length() != 0)
+//            editText1.setText(Float.toString(Float.parseFloat(char0) * procent));
+//    }
 //
-//        } catch (Exception ex) {}
-    }
+//    @Override
+//    public void onNothingSelected(AdapterView<?> adapterView) {
+//
+//    }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+//    public void additionButton(View v) {
+//        String valueNum = editText.getText().toString();
+//        String valueNum1 = editText1.getText().toString();
+//        switch (v.getId()) {
+//            case R.id.btnchange:
+//                editText.setText(valueNum1);
+//                editText1.setText(Float.toString(Float.parseFloat(valueNum1) * procent));
+//                break;
+////            case R.id.btndelete:
+////                if (valueNum.length() != 0) {
+////                    editText.setText(valueNum.substring(0, valueNum.length() - 1));
+////                    editText1.setText(Float.toString(Float.parseFloat(editText.getText().toString()) * procent));
+////                }
+////                break;
+////            case R.id.btnc:
+////                editText.setText("");
+////                editText1.setText("");
+////                break;
+//        }
+//    }
 
-    }
+//    public void copyInBuffer(View v) {
+//        String message = "";
+//        switch (v.getId()) {
+//            case R.id.btncopyfirstfield:
+//                message = editText.getText().toString().trim();
+//                break;
+//            case R.id.btncopysecondfield:
+//                message = editText1.getText().toString().trim();
+//        }
+//        ClipData clipData = ClipData.newPlainText("text", message);
+//        clipboardManager.setPrimaryClip(clipData);
+//        Toast toast = Toast.makeText(this, "The copy was successful", Toast.LENGTH_LONG);
+//        toast.show();
+//    }
 
-    public void additionButton(View v) {
-        String valueNum = editText.getText().toString();
-        String valueNum1 = editText1.getText().toString();
-        switch (v.getId()) {
-            case R.id.btnchange:
-                editText.setText(valueNum1);
-                editText1.setText(Float.toString(Float.parseFloat(valueNum1) * procent));
-                break;
-            case R.id.btndelete:
-                if (valueNum.length() != 0) {
-                    editText.setText(valueNum.substring(0, valueNum.length() - 1));
-                    editText1.setText(Float.toString(Float.parseFloat(editText.getText().toString()) * procent));
-                }
-                break;
-            case R.id.btnc:
-                editText.setText("");
-                editText1.setText("");
-                break;
-        }
-    }
-
-    public void copyInBuffer(View v) {
-        String message = "";
-        switch (v.getId()) {
-            case R.id.btncopyfirstfield:
-                message = editText.getText().toString().trim();
-                break;
-            case R.id.btncopysecondfield:
-                message = editText1.getText().toString().trim();
-        }
-        ClipData clipData = ClipData.newPlainText("text", message);
-        clipboardManager.setPrimaryClip(clipData);
-        Toast toast = Toast.makeText(this, "The copy was successful", Toast.LENGTH_LONG);
-        toast.show();
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        String textValue = editText.getText().toString();
-        switch (v.getId()) {
-            case R.id.btn0:
-                textValue += "0";
-                break;
-            case R.id.btn000:
-                textValue += "000";
-                break;
-            case R.id.btn1:
-                textValue += "1";
-                break;
-            case R.id.btn2:
-                textValue += "2";
-                break;
-            case R.id.btn3:
-                textValue += "3";
-                break;
-            case R.id.btn4:
-                textValue += "4";
-                break;
-            case R.id.btn5:
-                textValue += "5";
-                break;
-            case R.id.btn6:
-                textValue += "6";
-                break;
-            case R.id.btn7:
-                textValue += "7";
-                break;
-            case R.id.btn8:
-                textValue += "8";
-                break;
-            case R.id.btn9:
-                textValue += "9";
-                break;
-            case R.id.btndot: {
-                if (!textValue.contains("."))
-                    textValue += ".";
-            }
-            default:
-                textValue += "";
-        }
-        editText.setText(textValue);
-        editText1.setText(Float.toString(Float.parseFloat(textValue) * procent));
-    }
+//    @Override
+//    public void onClick(View v) {
+//
+//        String textValue = editText.getText().toString();
+//        switch (v.getId()) {
+////            case R.id.btn0:
+////                textValue += "0";
+////                break;
+////            case R.id.btn000:
+////                textValue += "000";
+////                break;
+////            case R.id.btn1:
+////                textValue += "1";
+////                break;
+////            case R.id.btn2:
+////                textValue += "2";
+////                break;
+////            case R.id.btn3:
+////                textValue += "3";
+////                break;
+////            case R.id.btn4:
+////                textValue += "4";
+////                break;
+////            case R.id.btn5:
+////                textValue += "5";
+////                break;
+////            case R.id.btn6:
+////                textValue += "6";
+////                break;
+////            case R.id.btn7:
+////                textValue += "7";
+////                break;
+////            case R.id.btn8:
+////                textValue += "8";
+////                break;
+////            case R.id.btn9:
+////                textValue += "9";
+////                break;
+//            case R.id.btndot: {
+//                if (!textValue.contains("."))
+//                    textValue += ".";
+//            }
+//            default:
+//                textValue += "";
+//        }
+//        editText.setText(textValue);
+//        editText1.setText(Float.toString(Float.parseFloat(textValue) * procent));
+//    }
 
 
 }
--- app/src/main/java/com/example/myapplication/table.java
+++ app/src/main/java/com/example/myapplication/table.java
@@ -3,20 +3,70 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class table extends Fragment {
+    Button button0;
+    Button button000;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    Button buttonCancel;
+    Button buttonDelete;
+    Button buttonDot;
 
+    BlankViewModel mViewModel;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View viewHierarchy = inflater.inflate(R.layout.fragment_table, container, false);
+        button0 = (Button) viewHierarchy.findViewById(R.id.btn0);
+        button000 = (Button) viewHierarchy.findViewById(R.id.btn000);
+        button1 = (Button) viewHierarchy.findViewById(R.id.btn1);
+        button2 = (Button) viewHierarchy.findViewById(R.id.btn2);
+        button3 = (Button) viewHierarchy.findViewById(R.id.btn3);
+        button4 = (Button) viewHierarchy.findViewById(R.id.btn4);
+        button5 = (Button) viewHierarchy.findViewById(R.id.btn5);
+        button6 = (Button) viewHierarchy.findViewById(R.id.btn6);
+        button7 = (Button) viewHierarchy.findViewById(R.id.btn7);
+        button8 = (Button) viewHierarchy.findViewById(R.id.btn8);
+        button9 = (Button) viewHierarchy.findViewById(R.id.btn9);
+        buttonCancel = (Button) viewHierarchy.findViewById(R.id.btnc);
+        buttonDelete = (Button) viewHierarchy.findViewById(R.id.btndelete);
+        buttonDot = (Button) viewHierarchy.findViewById(R.id.btndot);
+
+        button0.setOnClickListener(item -> mViewModel.setNumber("0"));
+        button000.setOnClickListener(item -> mViewModel.setNumber("000"));
+        button1.setOnClickListener(item -> mViewModel.setNumber("1"));
+        button2.setOnClickListener(item -> mViewModel.setNumber("2"));
+        button3.setOnClickListener(item -> mViewModel.setNumber("3"));
+        button4.setOnClickListener(item -> mViewModel.setNumber("4"));
+        button5.setOnClickListener(item -> mViewModel.setNumber("5"));
+        button6.setOnClickListener(item -> mViewModel.setNumber("6"));
+        button7.setOnClickListener(item -> mViewModel.setNumber("7"));
+        button8.setOnClickListener(item -> mViewModel.setNumber("8"));
+        button9.setOnClickListener(item -> mViewModel.setNumber("9"));
+        buttonCancel.setOnClickListener(item -> mViewModel.clearTheField());
+        buttonDelete.setOnClickListener(item -> mViewModel.clearTheSymbol());
+        buttonDot.setOnClickListener(item -> mViewModel.setDot());
 
         return viewHierarchy;
+    }
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = ViewModelProviders.of(getActivity()).get(BlankViewModel.class);
     }
 }
